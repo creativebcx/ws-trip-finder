cef04acec0f0990fd9b74b5b9d023965bac3e5f4
creativebcx <ericdonley1@gmail.com>
2017-07-02 17:56:40 -0600

    update

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ mongoose.Promise = global.Promise;
 
 app.use('/trip-finder', tripFinderRouter);
 
+app.use(function(req,res,next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 let server;
 
 // run server function
