8a5d6b2eb9bedd1bc3d3a8c9fb20b824f4bf1c44
creativebcx <ericdonley1@gmail.com>
2017-07-03 14:27:35 -0600

    update

--- config.js
+++ config.js
@@ -2,16 +2,3 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                       global.DATABASE_URL ||
                       "mongodb://localhost/downloads/seed-data.json"
 exports.PORT = process.env.PORT || 8080;
-
-//mongoimport --db wstestdb --collection trippackages --drop --file ~/Downloads/seed-data.json --host ds145952.mlab.com --port 45952  -u test -p test
-
-// secure-bastion-80953
-// https://secure-bastion-80953.herokuapp.com/ | https://git.heroku.com/secure-bastion-80953.git
-
-
-// don't do login yourself - login with another service - twitch login - google login - fb login - gitHub login
-// forgot other password not of their own
-// basic password reset (levels of security)
-// proper hasing of passwords
-// where are your password tokens generated
-// security not just obscurity
