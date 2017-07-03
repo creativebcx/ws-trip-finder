065eec0d13777d51d00d5b67a5ef183019959a0d
creativebcx <ericdonley1@gmail.com>
2017-07-02 21:48:16 -0600

    update-after-config-change

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                       global.DATABASE_URL ||
-                      //"mongodb://localhost/downloads/seed-data.json"//
-                      "mongodb://test:test@ds145952.mlab.com:45952/wstestdb";
+                      "mongodb://localhost/downloads/seed-data.json"
 exports.PORT = process.env.PORT || 8080;
 
 //mongoimport --db wstestdb --collection trippackages --drop --file ~/Downloads/seed-data.json --host ds145952.mlab.com --port 45952  -u test -p test
